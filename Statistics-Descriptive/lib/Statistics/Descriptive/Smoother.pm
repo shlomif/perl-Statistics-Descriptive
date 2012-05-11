f2be0d394d56f8ff46873c32f98991a3f12aefbd
Shlomi Fish <shlomif@shlomifish.org>
2012-05-11 11:59:09 +0300

    Bump the version number to 3.0600.

--- Statistics-Descriptive/lib/Statistics/Descriptive/Smoother.pm
+++ Statistics-Descriptive/lib/Statistics/Descriptive/Smoother.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use Carp;
 
-our $VERSION = '3.0500';
+our $VERSION = '3.0600';
 
 sub instantiate {
     my ($class, $args) = @_;
--- Statistics-Descriptive/lib/Statistics/Descriptive/Smoother/Exponential.pm
+++ Statistics-Descriptive/lib/Statistics/Descriptive/Smoother/Exponential.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use base 'Statistics::Descriptive::Smoother';
     
-our $VERSION = '3.0500';
+our $VERSION = '3.0600';
     
 sub _new {
     my ($class, $args) = @_;
